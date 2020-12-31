02e4e3a98341781c3391b51506596b4a08548bdf
RNO <arnaud.juracek@gmail.com>
2020-12-31 17:29:45 +0100

    Refactor project to make it content agnostic

---
+++ bin/index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const fs = require('fs-extra')
+const path = require('path')
+const pkg = require('../package.json')
+
+const argv = require('minimist')(process.argv.slice(2), {
+  alias: {
+    h: 'help',
+    v: 'version',
+    o: 'output',
+    b: 'blueprint'
+  },
+  boolean: ['help', 'porcelain', 'version', 'progress', 'publish'],
+  string: ['blueprint', 'download', 'output', 'array', 'ignore']
+})
+
+if (argv.help) {
+  console.log(pkg.name)
+  console.log(pkg.description, '\n')
+  console.log(fs.readFileSync(path.join(__dirname, 'USAGE'), 'utf8'))
+  process.exit(0)
+}
+
+if (argv.version) {
+  console.log(pkg.version)
+  process.exit(0)
+}
+
+require('../lib')(Object.assign({}, argv, {
+  input: argv._[0] && path.join(process.cwd(), argv._[0])
+})).catch(e => {
+  console.error(e.message)
+  console.error(`\nTry '${pkg.name} --help'`)
+  process.exit(1)
+})
